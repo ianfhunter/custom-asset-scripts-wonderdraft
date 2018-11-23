aba0ff427bea2eb315ff85ed9280aafe8a2410ca
Ian Hunter <ianfhunter@gmail.com>
2018-11-23 19:22:59 +0000

    YML file is all powerful (#47)
    * now you can do the permutations using the map app (preview)
    
    * small indent fix

--- convertMap.py
+++ convertMap.py
@@ -9,6 +9,7 @@ from user_interface import *
 from tqdm import tqdm
 from config import *
 import random
+import math
 import itertools
 
 def convertMap(args, gui=False):
@@ -64,42 +65,63 @@ def generateMapVariants(args, gui=False):
         basefileext, _, s = splitPath(fn, no_cat=True)
 
 
-
         for theme in tf['themes']:
-            svg_out = svg_in
 
             reps = tf['replace'] if 'replace' in tf else []
             perms = tf['permute'] if 'permute' in tf else []
 
-
             old_path = os.path.join(fn)
             new_path = os.path.join(old_path.replace('templates', MAP_DIR+'/'+theme))
 
-            for replacement in tf['replace']:
-
-                old_color = tf['replace'][replacement]
-                try:
-                    replacement = tf['themes'][theme][replacement]
-                except KeyError:
-                    print("Was asked to replace '"+replacement+"', but theme does not have any themes for it.")
-
-                if type(replacement) == str:
-                    # One-to-One replacement
-                    svg_out = replaceColor(svg_out, old_color, replacement)
-
-                elif type(replacement) == list:
-                    # Shuffle replacement
-                    while old_color in svg_out:
-                        new_color = random.choice(replacement)
-                        svg_out = replaceColor(svg_out, old_color, new_color, amount=1)
-                else:
-                    print("Err: Unsupported Type in Theme")
+            perm__ = []
+            if len(perms) > 0:
+                perm_category = list(perms.keys())[0]
+                permlist = perms[perm_category]
+                if(len(permlist) > 7):
+                    print("There are", math.factorial(len(permlist))), "different svgs to produe for one theme. Avoiding computer overload and quitting..."
+                    print("Please rethink your theme")
                     quit()
+                else:
+                    perm__ = list(itertools.permutations(permlist))                    
+            else:
+                perm__ = [None]
 
-                old_path = os.path.join(fn)
-                new_path = os.path.join(old_path.replace('templates', MAP_DIR+'/'+theme))
 
-                saveWrite(svg_out, new_path)
+            for p in perm__:
+
+                svg_out = svg_in
+
+                if p is not None:
+                    replaceMe = tf['themes'][theme][perm_category]
+                    for new, old in zip(replaceMe, p):
+                        svg_out = replaceColor(svg_out, old, new)
+                
+                if 'replace' in tf:
+                    for replacement in tf['replace']:
+
+                        old_color = tf['replace'][replacement]
+                        try:
+                            replacement = tf['themes'][theme][replacement]
+                        except KeyError:
+                            print("Was asked to replace '"+replacement+"', but theme does not have any themes for it.")
+
+                        if type(replacement) == str:
+                            # One-to-One replacement
+                            svg_out = replaceColor(svg_out, old_color, replacement)
+
+                        elif type(replacement) == list:
+                            # Shuffle replacement
+                            while old_color in svg_out:
+                                new_color = random.choice(replacement)
+                                svg_out = replaceColor(svg_out, old_color, new_color, amount=1)
+                        else:
+                            print("Err: Unsupported Type in Theme")
+                            quit()
+
+                    saveWrite(svg_out, new_path)
+                else:
+                    saveWrite(svg_out, new_path)
+                    
 
     return True
 
